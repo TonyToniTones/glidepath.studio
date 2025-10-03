83c2a1667c1c5177d3e8ca2cc636a34ef6495d1a
Anthony Duong <adduong@mac.home>
2025-10-03 14:07:18 -0400

    Add Vercel adapter for server output and middleware support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,7 @@
-// @ts-check
-
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
 
-// https://astro.build/config
 export default defineConfig({
-	output: 'server',
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  output: 'server',
+  adapter: vercel(),
 });
